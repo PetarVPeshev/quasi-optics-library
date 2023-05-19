7254c0276f841005f07a09aad2b1491e92f7e217
Petar V Peshev <40143832+PetarVPeshev@users.noreply.github.com>
2023-05-19 20:36:21 +0200

    Updated array impedance and bf current functions

--- array_impedance.m
+++ array_impedance.m
@@ -1,16 +1,43 @@
-function imped = array_impedance(dipole_l, dipole_w, dx, dy, ...
-    k, km_comp, sgf)
+function Zin = array_impedance(relat_permit, dipole_l, dipole_w, dx, dy, ...
+    k0, k_comp)
 %ARRAY_IMPEDANCE Summary of this function goes here
 %   Detailed explanation goes here
     % dipoles are oriented along x-axis (SGFxx)
-    I = ( 2 * k .* (cos(km_comp(:, :, :, 1) * dipole_l / 2) ...
-        - cos(k * dipole_l / 2)) ) ...
-        ./ ( (k ^ 2 - km_comp(:, :, :, 1) .^ 2) ...
-        * sin(k * dipole_l / 2) );
-    J = sinc( km_comp(:, :, :, 2) * dipole_w / (2 * pi) );
+    wave_impedance = 376.730313668 / sqrt(relat_permit);
+    k_comp = k_comp * sqrt(relat_permit);
+    k = k0 * sqrt(relat_permit);
 
-    prod = permute(sgf(:, :, :, 1, 1) .* (abs(I) .^ 2) ...
-        .* (abs(J) .^ 2), [3, 4, 1, 2]);
-    imped = permute(- sum(prod) / (dx * dy), [3, 4, 1, 2]);
+    mx_max = 40;
+    my_max = 40;
+    Zin = zeros( [size(k_comp, 1, 2)] );
+    for mx = -mx_max : 1 : mx_max
+        for my = -my_max : 1 : my_max
+            kxm = k_comp(:, :, 1) - 2 * pi * mx / dx;
+            kym = k_comp(:, :, 2) - 2 * pi * my / dy;
+            kzm = - 1j * sqrt(- k ^ 2 + kxm .^ 2 + kym .^ 2);
+
+            I = 2 * k ...
+                * (cos(kxm * dipole_l / 2) - cos(k * dipole_l / 2)) ...
+                ./ ((k ^ 2 - kxm .^ 2) * sin(k * dipole_l / 2));
+            J = sinc(kym * dipole_w / (2 * pi));
+            Gxx = - wave_impedance .* (k ^ 2 - kxm .^ 2) ...
+                ./ (2 * k * kzm);
+
+            Zin = Zin + Gxx .* (abs(I) .^ 2) .* (abs(J) .^ 2);
+        end
+    end
+    Zin = - Zin / (dx * dy);
 end
+% 
+% Zin = array_impedance(dipole_l, dipole_w, dx, dy, ...
+%     k, km_comp, sgf, k0, k_comp)
+%     I = ( 2 * k .* (cos(km_comp(:, :, :, 1) * dipole_l / 2) ...
+%         - cos(k * dipole_l / 2)) ) ...
+%         ./ ( (k ^ 2 - km_comp(:, :, :, 1) .^ 2) ...
+%         * sin(k * dipole_l / 2) );
+%     J = sinc( km_comp(:, :, :, 2) * dipole_w / (2 * pi) );
+% 
+%     prod = permute(sgf(:, :, :, 1, 1) .* (abs(I) .^ 2) ...
+%         .* (abs(J) .^ 2), [3, 4, 1, 2]);
+%     Zin = permute(- sum(prod) / (dx * dy), [3, 4, 1, 2]);
 
--- bf_current.m
+++ bf_current.m
@@ -11,19 +11,35 @@ function ibf = bf_current(relat_permit, dipole_l, dipole_w, dx, dy, k0, ...
     
     [k_comp, k] = wave_vector(relat_permit, k0, sph_grid_p);
 
-    B = 2 * k * ...
-        (cos(- k_comp(:, :, 1) * dipole_l / 2) - cos(k * dipole_l / 2)) ...
-        .* sinc(- k_comp(:, :, 2) * dipole_w / (2 * pi)) ...
-        ./ ( (k ^ 2 - (- k_comp(:, :, 1)) .^ 2) * sin(k * dipole_l / 2) );
-    voltage = (- sin(sph_grid_p(:, :, 2)) * te_amplitude ...
-        + cos(sph_grid_p(:, :, 2)) * tm_amplitude) .* B / sqrt(dx * dy);
+%     k = k0 * sqrt(relat_permit);
+%     kx = k * sin(sph_grid_p(:, :, 1)) .* cos(sph_grid_p(:, :, 2));
+%     ky = k * sin(sph_grid_p(:, :, 1)) .* sin(sph_grid_p(:, :, 2));
 
-    [km_comp, ~, ~] = floquet_modes(k, relat_permit, dx, dy, sph_grid_p);
-    sgf = floquet_sgf(relat_permit, k, km_comp);
-    impedance = array_impedance(dipole_l, dipole_w, dx, dy, k, km_comp, ...
-        sgf);
+    kx = k_comp(:, :, 1);
+    ky = k_comp(:, :, 2);
 
-    ibf = voltage ./ impedance;
+    I = 2 * k ...
+        * (cos( - kx * dipole_l / 2) - cos(k * dipole_l / 2)) ...
+        ./ ((k ^ 2 - ( - kx) .^ 2) * sin(k * dipole_l / 2));
+    J = sinc(- ky * dipole_w / (2 * pi));
+    B = I .* J;
+
+%     B = 2 * k * ...
+%         (cos(- k_comp(:, :, 1) * dipole_l / 2) - cos(k * dipole_l / 2)) ...
+%         .* sinc(- k_comp(:, :, 2) * dipole_w / (2 * pi)) ...
+%         ./ ( (k ^ 2 - (- k_comp(:, :, 1)) .^ 2) * sin(k * dipole_l / 2) );
+    voltage = ((- sin(sph_grid_p(:, :, 2)) * te_amplitude ...
+        + cos(sph_grid_p(:, :, 2)) * tm_amplitude) / sqrt(dx * dy)) .* B;
+
+%     [km_comp, ~, ~] = floquet_modes(k, relat_permit, dx, dy, sph_grid_p);
+%     sgf = floquet_sgf(relat_permit, k, km_comp);
+%     impedance = array_impedance(dipole_l, dipole_w, dx, dy, k, km_comp, ...
+%         sgf);
+    
+    Zin = array_impedance(relat_permit, dipole_l, dipole_w, dx, dy, ...
+        k0, k_comp);
+
+    ibf = voltage ./ Zin;
 end
 
 %     k = k0 * sqrt(relat_permit);
